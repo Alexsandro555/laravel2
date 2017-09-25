f61a03866adf69102d4eeb0b456255b02cf7bc97
Alexsandro <alexsandro@iMac.local>
2017-09-25 14:01:38 +0300

    Second commit

--- app/Product.php
+++ app/Product.php
@@ -8,54 +8,74 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Product extends Model
 {
-    use SoftDeletes;
+  use SoftDeletes;
 
-    protected $dates = ['deleted_at'];
+  protected $dates = ['deleted_at'];
 
-    protected $fillable = [
-        'id',
-        'title',
-        'url_key',
-        'price',
-        'description',
-        'qty',
-        'active',
-        'sort',
-        'onsale',
-        'special',
-        'need_order',
-        'category_id',
-        'type_product_id',
-        'producer_type_product_id',
-        'producer_id',
-        'article',
-        'IEC'
-    ];
+  protected $fillable = [
+      'id',
+      'title',
+      'url_key',
+      'price',
+      'description',
+      'qty',
+      'active',
+      'sort',
+      'onsale',
+      'special',
+      'need_order',
+      'category_id',
+      'type_product_id',
+      'producer_type_product_id',
+      'producer_id',
+      'article',
+      'IEC'
+  ];
 
-    public function photo() {
-        return $this->hasMany(ProductPhoto::class, 'product_id','id');
-    }
+  public function photo() {
+      return $this->hasMany(ProductPhoto::class, 'product_id','id');
+  }
 
-    public function type_product() {
-        return $this->belongsTo('App\TypeProduct');
-    }
+  public function type_product() {
+      return $this->belongsTo('App\TypeProduct');
+  }
 
-    public function category() {
-        return $this->belongsTo('App\Category');
-    }
+  public function category() {
+      return $this->belongsTo('App\Category');
+  }
 
-    public function attributes() {
-        return $this->belongsToMany('App\Attribute')->withPivot('value');
-    }
+  public function attributes() {
+      return $this->belongsToMany('App\Attribute')->withPivot('value');
+  }
 
-    /**
-     * Получить все изображения продукта
-     */
-    public function files() {
-        return $this->morphMany('App\File', 'fileable');
-    }
+  /**
+   * Получить все изображения продукта
+   */
+  public function files() {
+      return $this->morphMany('App\File', 'fileable');
+  }
 
-    public function producer_type_product() {
-        return $this->belongsTo('App\ProducerTypeProduct');
-    }
+  public function producer_type_product() {
+      return $this->belongsTo('App\ProducerTypeProduct');
+  }
+
+  public function setTitleAttribute($value)
+  {
+    $this->attributes['title'] = strip_tags($value);
+  }
+
+  public function setPriceAttribute($value)
+  {
+    $this->attributes['price'] = strip_tags($value);
+  }
+
+  public function setArticleAttribute($value)
+  {
+    $this->attributes['article'] = strip_tags($value);
+  }
+
+  public function setIECAttribute($value)
+  {
+    $this->attributes['IEC'] = strip_tags($value);
+  }
 }
